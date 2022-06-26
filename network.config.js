60a7fc2a67c491f60d8414d0767472c4d5a0e163
chainxvi <chainxvi@gmail.com>
2022-06-26 13:47:46 +0200

    fixes

--- network.config.js
+++ network.config.js
@@ -4,7 +4,6 @@ module.exports = {
     4: {
       name: 'rinkeby',
       priceFeed: '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e',
-      deployed: true,
     },
   },
   devChains: ['hardhat', 'localhost']
